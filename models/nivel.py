e7d087157ad4c36cb56cd39186756412ec33c531
2dam <2dam@MACINF01.TartangaLH.eus>
2020-01-30 12:19:50 +0100

    model, constraint y report.

--- models/nivel.py
+++ models/nivel.py
@@ -9,5 +9,21 @@ class Nivel(models.Model):
     nivel_academico = fields.Char(string = "Nivel academico")
     espeificacion = fields.Char(string = "Especificacion")
     descripcion = fields.Text()
+    #onde ondelete = 'cascade'
+    profesional_id = fields.One2many('res.partner', 'nivel_id', string = "Profesional" )
     
-    #profesional_id = fields.One2many('verificaciones_de_productos.profesional', ondelete = 'cascade', string = "Profesional" )
+    @api.constrains('nivel_academico')
+    def _verificar_nivel_academico(self):
+        if (len(self.nivel_academico) < 3):
+            raise exceptions.ValidationError("Nivel academico minimo longitud 4 caracteres.")
+    
+    
+    @api.constrains('espeificacion')
+    def _verificar_espeificacion(self):
+        if len(self.espeificacion) < 3:
+            raise exceptions.ValidationError("Especificacion minimo longitud 4 caracteres.")
+        
+    @api.constrains('descripcion')
+    def _verificar_descripcion_nivel(self):
+        if self.descripcion and (len(self.descripcion) < 3):
+            raise exceptions.ValidationError("Descripcion minimo longitud 4 caracteres.")
